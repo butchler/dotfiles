427f64e9b65844303647458e2515d48c193b64ae
Adam Buechler <butchler@gmail.com>
2022-09-21 19:14:30 +0900

    Fix stowing for neovim

--- stow.sh
+++ stow.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-stow -v neovim
+mkdir -pv ~/.config; stow -v neovim
 stow -v git
 stow -v nix-darwin
 stow -v zsh
 stow -v tmux
-mkdir -p ~/code; stow -v project-scripts
+mkdir -pv ~/code; stow -v project-scripts
 stow -v alacritty
