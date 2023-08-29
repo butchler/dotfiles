581e6bfce8f89bcdfb3ba91c08e6ca239700372e
Adam Buechler <butchler@gmail.com>
2023-08-29 13:37:51 +0900

    Remove project scripts

--- stow.sh
+++ stow.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
+mkdir -pv ~/code
 mkdir -pv ~/.config; stow -v neovim
 stow -v git
 stow -v nix-darwin
 stow -v zsh
 stow -v tmux
-mkdir -pv ~/code; stow -v project-scripts
 stow -v alacritty
